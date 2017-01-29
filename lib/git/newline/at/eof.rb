62919932bf2b03e1fec6b65c1d45b3345d9cf994
Code Ass <aycabta@gmail.com>
2017-01-29 23:08:07 +0900

    Initial commit

---
+++ lib/git/newline/at/eof.rb
@@ -0,0 +1,11 @@
+require "git/newline/at/eof/version"
+
+module Git
+  module Newline
+    module At
+      module Eof
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/git/newline/at/eof/version.rb
@@ -0,0 +1,9 @@
+module Git
+  module Newline
+    module At
+      module Eof
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
