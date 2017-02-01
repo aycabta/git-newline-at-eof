26e5b350f3a919ccbe37a371c5eb718615c2ba59
Code Ass <aycabta@gmail.com>
2017-02-01 21:41:25 +0900

    Add CLI test for --feed-last-line

---
+++ test/cli_test.rb
@@ -0,0 +1,49 @@
+require 'helper'
+require 'tmpdir'
+
+class GitNewlineAtEof::Test < Test::Unit::TestCase
+  def setup
+    @tmpdir = Dir.mktmpdir
+  end
+  def teardown
+    FileUtils.remove_entry_secure(@tmpdir)
+  end
+  sub_test_case ' with --feed-last-line' do
+    setup do
+      create_file(@tmpdir, 'file0') do |f|
+        f.write('abc')
+      end
+      create_file(@tmpdir, 'file1') do |f|
+        f.write("line\n")
+      end
+      create_file(@tmpdir, 'file2') do |f|
+        f.write("line\n\n")
+      end
+      create_file(@tmpdir, 'file3') do |f|
+      end
+      Dir.chdir(@tmpdir)
+      `git init`
+      `git add .`
+      `git commit -m 'Initial commit'`
+    end
+    test 'requests the correct resource' do
+      result = `#{cli_cmd} --check-all`
+      assert_equal_message(result) do
+        <<~EOM
+        file0: no newline at end of file
+        file2: discarded 1 newline at end of file
+        file3: no newline at end of file
+        EOM
+      end
+      result = `#{cli_cmd} --feed-last-line`
+      assert_equal(result, '')
+      assert_equal($?, 0)
+      result = `#{cli_cmd} --check-all`
+      assert_equal_message(result) do
+        <<~EOM
+        file2: discarded 1 newline at end of file
+        EOM
+      end
+    end
+  end
+end
--- test/helper.rb
+++ test/helper.rb
@@ -1,1 +1,16 @@
 require 'test-unit'
+
+def create_file(dir, filename)
+  filepath = File.join(dir, filename)
+  File.open(filepath, 'w') do |f|
+    yield f if block_given?
+  end
+end
+
+def cli_cmd
+  File.expand_path('../../exe/git-newline-at-eof', __FILE__)
+end
+
+def assert_equal_message(result)
+  assert_equal(result, yield)
+end
