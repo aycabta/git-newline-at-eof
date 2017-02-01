5a537f0698004761c2031beb13d4454c520678d9
Code Ass <aycabta@gmail.com>
2017-02-01 21:57:09 +0900

    Add test for --discard-last-newline

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -39,4 +39,36 @@ class GitNewlineAtEof::Test < Test::Unit::TestCase
       end
     end
   end
+  sub_test_case ' with --discard-last-newline' do
+    setup do
+      create_file(@tmpdir, 'file0', 'abc')
+      create_file(@tmpdir, 'file1', "line\n")
+      create_file(@tmpdir, 'file2', "line\n\n")
+      create_file(@tmpdir, 'file3', '')
+      Dir.chdir(@tmpdir)
+      `git init`
+      `git add .`
+      `git commit -m 'Initial commit'`
+    end
+    test 'requests the correct resource' do
+      result = `#{cli_cmd} --check-all`
+      assert_equal_message(result) do
+        <<~EOM
+        file0: no newline at end of file
+        file2: discarded 1 newline at end of file
+        file3: no newline at end of file
+        EOM
+      end
+      result = `#{cli_cmd} --discard-last-newline`
+      assert_equal(result, '')
+      assert_equal($?, 0)
+      result = `#{cli_cmd} --check-all`
+      assert_equal_message(result) do
+        <<~EOM
+        file0: no newline at end of file
+        file3: no newline at end of file
+        EOM
+      end
+    end
+  end
 end
