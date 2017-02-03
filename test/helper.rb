27a8a75879e9f47d4e78315aebbf5ab131cc31d4
Code Ass <aycabta@gmail.com>
2017-02-03 23:08:40 +0900

    Use ruby command for exec CLI in test

--- test/helper.rb
+++ test/helper.rb
@@ -8,7 +8,8 @@ def create_file(dir, filename, substance)
 end
 
 def cli_cmd
-  File.expand_path('../../exe/git-newline-at-eof', __FILE__)
+  expanded_path = File.expand_path('../../exe/git-newline-at-eof', __FILE__)
+  "ruby #{expanded_path}"
 end
 
 def assert_equal_message(result)
