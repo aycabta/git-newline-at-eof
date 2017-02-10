28faf656cb2e91cea61bafc01f314e56b44b9f18
Code Ass <aycabta@gmail.com>
2017-02-10 20:41:20 +0900

    Add comment for multi byte

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -124,8 +124,8 @@ class GitNewlineAtEof::Test < Test::Unit::TestCase
       create_file(@tmpdir, 'file0', 'abc')
       create_file(@tmpdir, 'file1', "line\n")
       create_file(@tmpdir, 'file2', "line\n\n")
-      create_file(@tmpdir, 'file3', '黒須')
-      create_file(@tmpdir, 'file4', '白玉')
+      create_file(@tmpdir, 'file3', '黒須') # multi byte char
+      create_file(@tmpdir, 'file4', '白玉') # multi byte char
       create_file(@tmpdir, 'file5', (0x00..0xFF).map(&:chr).concat(["\n"] * 10).join)
       `git init`
       `git add .`
